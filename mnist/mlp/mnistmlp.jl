761731477975e3dae820b96d3952a816ec4d3296
zlw <zhulunwu@outlook.com>
2020-10-10 14:04:52 +0800

    mnistmlp

--- mnist/mlp/mnistmlp.jl
+++ mnist/mlp/mnistmlp.jl
@@ -1,31 +1,46 @@
 using Flux
-using Flux: onehotbatch, crossentropy
+using Flux: onehot,crossentropy,chunk,batch,throttle
+using Flux: @epochs
+using Statistics
 
 include("data.jl")
+
+# 图像转一维向量
+function img2vec(image)
+    f=map(Float32,image)
+    return vcat(f...) 
+end
+
 # 读入图像和标签
-images = minst_images()
-labels = minst_labels()
-dataset=zip(images,labels)
+images = img2vec.(minst_images())
+labels = map(x->onehot(x,0:9),minst_labels())
 
-function nextbatch(batchsize::Int)
-    data=rand(collect(dataset),batchsize)
-    imgs=[Float32.(data[i][1]) for i=1:batchsize]
-    imgs = hcat(reshape.(imgs, :)...)
-    lbls=[data[i][2] for i=1:batchsize]
-    lbls=onehotbatch(lbls,0:9)
-    return imgs,lbls
+nparts=6000
+images_batch=chunk(images,nparts)
+labels_batch=chunk(labels,nparts)
+images_matrix=batch.(images_batch)
+labels_matrix=batch.(labels_batch)
+
+dataset=zip(images_matrix,labels_matrix)
+
+m=Chain(Dense(28^2,64,relu),Dense(64,10),softmax)
+
+loss(x,y)=crossentropy(m(x),y)
+
+function showloss()
+    selection=rand(1:60000,10)
+    losssum=0
+    for s in selection
+        losssum+=loss(images[s],labels[s])
+    end
+    lossaverage=losssum*0.1
+    print("\raverage loss=",lossaverage)
 end
 
-m=Chain(Dense(28^2,128,relu),Dense(128,10),softmax)
+@epochs 5 Flux.train!(loss, params(m),dataset,RMSProp(0.0001),cb=throttle(showloss,1))
 
-function loss(x,y)
-    l=crossentropy(m(x),y)
-    println("loss=",l)
-    return l
-end
-
-# 训练
-for step=1:1000
-    imgs,lbls=nextbatch(32)
-    Flux.train!(loss, params(m),[(imgs,lbls)],RMSProp(0.0001))
-end
+# 模型的使用
+image_test = img2vec.(minst_images(:test))
+label_test = minst_labels(:test)
+predicts=argmax.(m.(image_test)).-1
+mean(label_test .== predicts)
